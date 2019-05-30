9587760e467577728166282e23f0d2cdc00891e0
刘冠延 <13136386067@163.com>
2019-05-30 16:34:09 +0800

    update tab function

--- src/lib/tabs/tab-item/tab-item.component.ts
+++ src/lib/tabs/tab-item/tab-item.component.ts
@@ -3,13 +3,14 @@ import {
   ChangeDetectorRef,
   Component,
   ElementRef, EventEmitter,
-  Input,
+  Input, OnChanges, OnDestroy,
   OnInit, Output,
-  Renderer2,
+  Renderer2, SimpleChange, SimpleChanges,
   TemplateRef,
   ViewChild,
   ViewEncapsulation
 } from '@angular/core';
+import {Subject} from 'rxjs';
 
 @Component({
   selector: 'nd-tab-item',
@@ -19,7 +20,27 @@ import {
   changeDetection: ChangeDetectionStrategy.OnPush,
   encapsulation: ViewEncapsulation.None
 })
-export class NdTabItemComponent implements OnInit {
+export class NdTabItemComponent implements OnChanges, OnInit, OnDestroy {
+
+  /** Emits whenever the internal state of the tab changes. */
+  readonly _stateChanges = new Subject<void>();
+
+  /**
+   * The relatively indexed position where 0 represents the center, negative is left, and positive
+   * represents the right.
+   */
+  position: number | null = null;
+
+  /**
+   * The initial relatively index origin of the tab if it was created and selected after there
+   * was already a selected tab. Provides context of what position the tab should originate from.
+   */
+  origin: number | null = null;
+
+  /**
+   * Whether the tab is currently active.
+   */
+  isActive = false;
 
   private readonly el: HTMLElement;
   @ViewChild(TemplateRef) content: TemplateRef<void>;
@@ -36,7 +57,16 @@ export class NdTabItemComponent implements OnInit {
     this.el = this.elementRef.nativeElement;
   }
 
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes.hasOwnProperty('ndLabel') || changes.hasOwnProperty('ndDisabled')) {
+      this._stateChanges.next();
+    }
+  }
+
   ngOnInit() {
   }
 
+  ngOnDestroy() {
+    this._stateChanges.complete();
+  }
 }
--- src/lib/tabs/tabs.component.ts
+++ src/lib/tabs/tabs.component.ts
@@ -27,8 +27,16 @@ import {NdTabInkBarComponent} from './tab-header/ink-bar/ink-bar.component';
 import {RendererService} from '../core/service/renderer.service';
 import {NdTabHeaderComponent} from './tab-header/tab-header.component';
 import {NdTabLabelComponent} from './tab-header/tab-list/tab-label/tab-label.component';
-import {Observable, Subject} from 'rxjs';
-import {distinctUntilChanged} from 'rxjs/operators';
+import {merge, Subscription} from 'rxjs';
+import {coerceNumberProperty} from '@angular/cdk/coercion';
+
+/** A simple change event emitted on focus or selection changes. */
+export class NdTabChangeEvent {
+  /** Index of the currently-selected tab. */
+  index: number;
+  /** Reference to the currently-selected tab. */
+  tab: NdTabItemComponent;
+}
 
 @Component({
   selector: 'nd-tabs',
@@ -61,8 +69,26 @@ export class NdTabsComponent
   public tabItems: NdTabItemComponent[];
 
 
+  /** The tab index that should be selected after the content has been checked. */
+  private _indexToSelect: number | null = 0;
+
+  /** Subscription to tabs being added/removed. */
+  private _tabsSubscription = Subscription.EMPTY;
+
+  /** Subscription to changes in the tab labels. */
+  private _tabLabelSubscription = Subscription.EMPTY;
+
   @Input() ndPosition: TabPosition = 'top';
-  @Input() ndSelectedIndex: number = 0;
+  // @Input() ndSelectedIndex: number | null = null;
+
+  /** The index of the active tab. */
+  @Input()
+  get ndSelectedIndex(): number | null { return this._selectedIndex; }
+  set ndSelectedIndex(value: number | null) {
+    this._indexToSelect = coerceNumberProperty(value, null);
+  }
+  public _selectedIndex: number | null = null;
+
   @Input() ndLabelHeight: number;
   @Input() ndLabelWidth: number;
   @Input() ndLabelSize: TabSize = 'default';
@@ -94,6 +120,9 @@ export class NdTabsComponent
   @ContentChildren(NdTabItemComponent) _tabs: QueryList<NdTabItemComponent>;
 
   @Output() readonly ndSelectedIndexChange: EventEmitter<number> = new EventEmitter<number>();
+  /** Event emitted when the tab selection has changed. */
+  @Output() readonly ndSelectedTabChange: EventEmitter<NdTabChangeEvent> =
+    new EventEmitter<NdTabChangeEvent>(true);
 
   constructor(
     private elementRef: ElementRef,
@@ -106,14 +135,81 @@ export class NdTabsComponent
   ngOnInit() {
   }
 
-  ngAfterContentChecked() {}
+  ngAfterContentChecked() {
+    // Don't clamp the `indexToSelect` immediately in the setter because it can happen that
+    // the amount of tabs changes before the actual change detection runs.
+    const indexToSelect = this._indexToSelect = this._clampTabIndex(this._indexToSelect);
+
+    // If there is a change in selected index, emit a change event. Should not trigger if
+    // the selected index has not yet been initialized.
+    if (this._selectedIndex != indexToSelect) {
+      const isFirstRun = this._selectedIndex == null;
+
+      if (!isFirstRun) {
+        this.ndSelectedTabChange.emit(this._createChangeEvent(indexToSelect));
+      }
+
+      // Changing these values after change detection has run
+      // since the checked content may contain references to them.
+      Promise.resolve().then(() => {
+        this._tabs.forEach((tab, index) => tab.isActive = index === indexToSelect);
+
+        if (!isFirstRun) {
+          this.ndSelectedIndexChange.emit(indexToSelect);
+        }
+      });
+    }
+
+    // Setup the position for each tab and optionally setup an origin on the next selected tab.
+    this._tabs.forEach((tab: NdTabItemComponent, index: number) => {
+      tab.position = index - indexToSelect;
+
+      // If there is already a selected tab, then set up an origin for the next selected tab
+      // if it doesn't have one already.
+      if (this._selectedIndex != null && tab.position == 0 && !tab.origin) {
+        tab.origin = indexToSelect - this._selectedIndex;
+      }
+    });
+
+    if (this._selectedIndex !== indexToSelect) {
+      this._selectedIndex = indexToSelect;
+      this.changeDetectorRef.markForCheck();
+    }
+  }
 
   ngAfterContentInit() {
     if (this.ndInkBarColor) {
       RendererService.updateStyles(this.el, {'background-color': this.ndInkBarColor}, this.renderer);
     }
-    this.changeDetectorRef.detectChanges();
+
     this.tabItems = this._tabs.toArray();
+
+    this._subscribeToTabLabels();
+
+    // Subscribe to changes in the amount of tabs, in order to be
+    // able to re-render the content as new tabs are added or removed.
+    this._tabsSubscription = this._tabs.changes.subscribe(() => {
+      const indexToSelect = this._clampTabIndex(this._indexToSelect);
+
+      // Maintain the previously-selected tab if a new tab is added or removed and there is no
+      // explicit change that selects a different tab.
+      if (indexToSelect === this._selectedIndex) {
+        const tabs = this._tabs.toArray();
+
+        for (let i = 0; i < tabs.length; i++) {
+          if (tabs[i].isActive) {
+            // Assign both to the `_indexToSelect` and `_selectedIndex` so we don't fire a changed
+            // event, otherwise the consumer may end up in an infinite loop in some edge cases like
+            // adding a tab within the `selectedIndexChange` event.
+            this._indexToSelect = this._selectedIndex = i;
+            break;
+          }
+        }
+      }
+
+      this._subscribeToTabLabels();
+      this.changeDetectorRef.markForCheck();
+    });
   }
 
   ngAfterViewInit() {
@@ -139,6 +235,22 @@ export class NdTabsComponent
       this.setInkBarPosition(idx);
     }
   }
+
+  /**
+   * Subscribes to changes in the tab labels. This is needed, because the @Input for the label is
+   * on the MatTab component, whereas the data binding is inside the MatTabGroup. In order for the
+   * binding to be updated, we need to subscribe to changes in it and trigger change detection
+   * manually.
+   */
+  private _subscribeToTabLabels() {
+    if (this._tabLabelSubscription) {
+      this._tabLabelSubscription.unsubscribe();
+    }
+
+    this._tabLabelSubscription = merge(...this._tabs.map(tab => tab._stateChanges))
+      .subscribe(() => this.changeDetectorRef.markForCheck());
+  }
+
 
   private setInkBarPosition(idx: number): void {
     // tslint:disable-next-line:no-string-literal
@@ -221,6 +333,25 @@ export class NdTabsComponent
   }
 
 
+  /** Clamps the given index to the bounds of 0 and the tabs length. */
+  private _clampTabIndex(index: number | null): number {
+    // Note the `|| 0`, which ensures that values like NaN can't get through
+    // and which would otherwise throw the component into an infinite loop
+    // (since Math.max(NaN, 0) === NaN).
+    return Math.min(this._tabs.length - 1, Math.max(index || 0, 0));
+  }
+
+  private _createChangeEvent(index: number): NdTabChangeEvent {
+    const event = new NdTabChangeEvent();
+    event.index = index;
+    if (this._tabs && this._tabs.length) {
+      event.tab = this._tabs.toArray()[index];
+    }
+    return event;
+  }
+
   ngOnDestroy() {
+    this._tabsSubscription.unsubscribe();
+    this._tabLabelSubscription.unsubscribe();
   }
 }
